36cc5c100370cbb7db4c62da818dd28913e6e718
Arun Raghavan <arun@asymptotic.io>
2025-04-30 22:30:39 -0400

    spa: Add Dict type alias
    We'll represent spa_dict with just a standard HashMap<String, String>

--- spa/src/lib.rs
+++ spa/src/lib.rs
@@ -2,5 +2,9 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025 Asymptotic Inc.
 // SPDX-FileCopyrightText: Copyright (c) 2025 Arun Raghavan
 
+use std::collections::HashMap;
+
 pub mod param;
 pub mod pod;
+
+pub type Dict = HashMap<String, String>;
