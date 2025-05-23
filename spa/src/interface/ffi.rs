40ddcb86693e904273267e3206437513a520fa6d
Sanchayan Maity <sanchayan@asymptotic.io>
2025-05-23 13:17:54 +0530

    spa: Implement missing bits for loop interface

--- spa/src/interface/ffi.rs
+++ spa/src/interface/ffi.rs
@@ -2,7 +2,7 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025 Asymptotic Inc.
 // SPDX-FileCopyrightText: Copyright (c) 2025 Arun Raghavan
 
-use std::ffi::{c_char, c_void};
+use std::ffi::{c_char, c_int, c_void};
 
 /* spa_callbacks */
 #[repr(C)]
@@ -26,3 +26,54 @@ pub struct CSupport {
     pub type_: *mut c_char,
     pub data: *mut c_void,
 }
+
+/* spa_list */
+#[repr(C)]
+pub struct CList {
+    pub next: *mut CList,
+    pub prev: *mut CList,
+}
+
+/* spa_hook_list */
+#[repr(C)]
+pub struct CHookList {
+    pub list: CList,
+}
+
+/* spa_hook */
+#[repr(C)]
+pub struct CHook {
+    pub link: CList,
+    pub cb: CCallbacks,
+    pub removed: extern "C" fn(hook: *mut CHook),
+    pub priv_: *mut c_void,
+}
+
+#[repr(C)]
+pub struct CControlHooks {
+    pub version: u32,
+    pub before: extern "C" fn(data: *mut c_void),
+    pub after: extern "C" fn(data: *mut c_void),
+}
+
+#[repr(C)]
+pub struct CLoop {
+    pub iface: CInterface,
+}
+
+#[repr(C)]
+pub struct CSource {
+    /*
+     * this should be *mut, but mutable dererefence via dyn Any
+     * (when used as CLoopImpl inner) does not work
+     */
+    pub loop_: *const CLoop,
+    pub func: CSourceFunc,
+    pub data: *mut c_void,
+    pub fd: c_int,
+    pub mask: u32,
+    pub rmask: u32,
+    pub priv_: *mut c_void,
+}
+
+pub type CSourceFunc = extern "C" fn(source: *mut CSource);
